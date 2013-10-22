d65f2145af4e8bbeb98b222d1794e6dad7a6d981
vagmi <vagmi.mudumbai@gmail.com>
2013-10-22 16:54:42 +0530

    build a better gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,63 @@
 var grunt = require('grunt');
 grunt.initConfig({
-    'http-server':
-
-            {
-                root: ".",
-
-                port: 8282,
-                host: "127.0.0.1",
-
-                showDir : true,
-                autoIndex: true,
-                defaultExt: "html",
-
-                //wait or not for the process to finish
-                runInBAckground: false
-            }
+  jshint: {
+    options: {
+      curly: true,
+      eqeqeq: true,
+      eqnull: true,
+      browser: true,
+      globals: {
+        jQuery: true
+      },
+    },
+    uses_defaults: ['app/**/*.js'],
+  },
+  copy: {
+    app: {
+      files: [
+        {expand: true, src: 'app/**', dest: './build'},
+        {expand: true, src: 'index.html', dest: './build/'},
+      ]
+    },
+    lib: {
+      files: [
+        {expand: true, src: 'lib/**', dest: './build'},
+      ]
+    },
+    styles: {
+      files: [
+        {expand: true, src: 'styles/**', dest: './build'},
+      ]
+    }
+  },
+  connect: {
+    server: {
+      options: {
+        port: 9001,
+        base: 'build',
+        livereload: true,
+        open: true
+      }
+    }
+  },
+  watch: {
+    app: {
+      files: 'app/**',
+      tasks: ['copy'],
+    },
+    build : {
+      files: 'build/**',
+      tasks: [],
+      options: {
+        livereload: true,
+      },
+    }
+}
 
 });
-grunt.loadNpmTasks('grunt-http-server');
+grunt.loadNpmTasks("grunt-contrib-jshint");
+grunt.loadNpmTasks("grunt-contrib-copy");
+grunt.loadNpmTasks("grunt-contrib-connect");
+grunt.loadNpmTasks("grunt-contrib-watch");
+
+grunt.registerTask('dev', ['copy','connect','watch']);
